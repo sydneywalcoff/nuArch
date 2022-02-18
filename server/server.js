e38b19db6be08ff302ec51f1b20cfa6e36ebe8c9
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-18 10:12:54 -0800

    add gitignore, npm init, add express and nodemon, establish server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Now listening at PORT ${PORT}`);
+})
+
