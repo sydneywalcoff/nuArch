5e6164c04d719f83437702907d6c842408b2626f
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-22 10:35:14 -0800

    create mongodb connection and add to server

--- server/server.js
+++ server/server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 
 const app = express();
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-app.listen(PORT, () => {
-    console.log(`Now listening at PORT ${PORT}`);
-});
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Now listening at PORT ${PORT}`);
+    });
+})
