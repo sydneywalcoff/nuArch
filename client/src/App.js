2a09c8282908542b5b8072e1195efb75df351f73
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-23 15:00:31 -0800

    basic todo component modularized and now it adds todos :)

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,10 @@
 import "./App.css";
+import Todo from './components/Todo';
 
 function App() {
-  const todos = [
-    {
-      todo: "walk dog",
-      completed: false,
-    },
-    {
-      todo: "do dishes",
-      completed: true,
-    },
-  ];
-
-  const statusHandler = (todo) => {
-    if(todo.completed === false) return '[ ]';
-    else return '[x]'
-  };
-
   return (
     <main className="wrapper">
-      <div className="todo-container">
-        <h2 className="container-title">Todos</h2>
-        <table>
-          <thead>
-            <tr>
-              <th scope="col">Status</th>
-              <th scope="col">Todo</th>
-            </tr>
-          </thead>
-          <tbody>
-            {todos.map((todo, i) => (
-              <tr key={i}>
-                <td>
-                  {statusHandler(todo)}
-                </td>
-                <td>{todo.todo}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <form action="submit">
-          <label htmlFor="input">add todo:</label>
-          <input name="input" type="text" />
-          <button type="submit">+</button>
-        </form>
-      </div>
+      <Todo />
     </main>
   );
 }
---
+++ client/src/components/Todo.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+const Todo = () => {
+  const [inputValue, setInputValue] = useState("");
+  const initialTodos = [
+    {
+      todo: "walk dog",
+      completed: false,
+    },
+    {
+      todo: "do dishes",
+      completed: true,
+    },
+  ];
+  const [todos, setTodos] = useState(initialTodos);
+
+  const statusHandler = (todo) => {
+    if (todo.completed === false) return "[ ]";
+    else return "[x]";
+  };
+
+  const changeHandler = (e) => [setInputValue(e.target.value)];
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const todoObj = {
+      todo: inputValue,
+      completed: false,
+    };
+    const temp = todos;
+    temp.push(todoObj);
+    setTodos(temp);
+    setInputValue("");
+  };
+  return (
+    <div className="todo-container">
+      <h2 className="container-title">Todos</h2>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Status</th>
+            <th scope="col">Todo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo, i) => (
+            <tr key={i}>
+              <td>{statusHandler(todo)}</td>
+              <td>{todo.todo}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <form action="POST" onSubmit={submitHandler}>
+        <input
+          name="input"
+          type="text"
+          placeholder="add todo"
+          onChange={changeHandler}
+          value={inputValue}
+        />
+        <button>+</button>
+      </form>
+    </div>
+  );
+};
+
+export default Todo;
