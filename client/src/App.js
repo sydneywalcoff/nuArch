94ddbd2a4bf2b84624c79050d2cf015a892a1c6e
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-23 12:14:27 -0800

    basic table styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,32 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className='wrapper'>
-      <header className='header'>todo app</header>
-      <main>
-        <div className='todo-container'>
-          <h1 className='container-title'>Todos</h1>
+    return (
+        <div className="wrapper">
+            <header className="header">
+                <h1>todo app</h1>
+            </header>
+            <main>
+                <div className="todo-container">
+                    <h2 className="container-title">Todos</h2>
+                    <table>
+                      <tr>
+                        <th scope="col">Status</th>
+                        <th scope="col">Todo</th>
+                      </tr>
+                      <tr>
+                        <td>[ ]</td>
+                        <td>walk dog</td>
+                      </tr>
+                      <tr>
+                        <td>[x]</td>
+                        <td>do dishes</td>
+                      </tr>
+                    </table>
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  );
+    );
 }
 
 export default App;
